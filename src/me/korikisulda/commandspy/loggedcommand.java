5eaa14d97a1d91cfd801c1b3e29948e754f2192d
korikisulda <korikisulda@plugmania.info>
2012-06-11 17:59:08 +0100

    added configurable blacklist, perms fixed a bit

--- src/me/korikisulda/commandspy/loggedcommand.java
+++ src/me/korikisulda/commandspy/loggedcommand.java
@@ -23,74 +23,96 @@ public class loggedcommand {
 	public CuboidSelection DBselection;
 	public boolean isWorldedit;
 	public Location location;
-    public List<String> WorldEditcommands = Arrays.asList(new String[]{"//replace","//set","//regen","//walls"});
-    public boolean isDatabaseEntry=false;
-    public Timestamp timeExecuted;
-	public String getCommandStripped(){ return command.substring(1);}
-	
+	public List<String> WorldEditcommands = Arrays.asList(new String[] {
+			"//replace", "//set", "//regen", "//walls" });
+	public boolean isDatabaseEntry = false;
+	public Timestamp timeExecuted;
+
+	public String getCommandStripped() {
+		return command.substring(1);
+	}
+
 	private commandspy plugin;
-public loggedcommand(String Command,String Args,String username,String usergroup,String World,boolean wasdenied,Region isSelected,Location loc,commandspy Cspy){
-	
-	command=Command;
-	arguments=Args;
-	playername=username;
-	groupname=usergroup;
-	world=World;
-	denied=wasdenied;
-	location=loc;
-	selection=isSelected;
 
-	plugin=Cspy;
-	if(WorldEditcommands.contains(command.toLowerCase())&&isSelected!=null) isWorldedit=true;
-}
+	public loggedcommand(String Command, String Args, String username,
+			String usergroup, String World, boolean wasdenied,
+			Region isSelected, Location loc, commandspy Cspy) {
 
+		command = Command;
+		arguments = Args;
+		playername = username;
+		groupname = usergroup;
+		world = World;
+		denied = wasdenied;
+		location = loc;
+		selection = isSelected;
 
-public loggedcommand(loggedcommand Log,commandspy Cspy){
-	command=Log.command;
-	arguments=Log.arguments;
-	playername=Log.playername;
-	groupname=Log.groupname;
-	world=Log.world;
-	denied=Log.denied;
-	selection=Log.selection;
-	isWorldedit=Log.isWorldedit;
-	location=Log.location;
-	plugin=Cspy;
-}
-
-public loggedcommand(commandspy Cspy){
-	plugin=Cspy;
-}
-
-public void tellPlayer(Player user){
-	if(plugin.spylist.containsKey(playername.toLowerCase())){
- user.sendMessage(getPrefix() + ChatColor.AQUA + playername + ": "  +  command + " " + arguments) ;
-	}else if(plugin.getServer().getPlayerExact(playername).hasPermission("commandspy.toggle")||plugin.getServer().getPlayerExact(playername).hasPermission("commandspy.set")||plugin.debugUsers.contains(playername)){
- user.sendMessage(getPrefix() + ChatColor.DARK_AQUA + playername + ": " + command + " " + arguments) ;
-	}else{
- user.sendMessage(getPrefix() + ChatColor.YELLOW + playername + ": " + command + " " + arguments) ;
+		plugin = Cspy;
+		if (WorldEditcommands.contains(command.toLowerCase())
+				&& isSelected != null)
+			isWorldedit = true;
 	}
-}
 
-public void tellPlayerDB(Player user){
-	user.sendMessage(ChatColor.GRAY + new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(timeExecuted) + ChatColor.WHITE + playername + ": " + command + " " + arguments);
-}
-
-
-public String getPrefix(){
-	try{
-	if(command.startsWith("//")){
-			return ChatColor.YELLOW + "[WorldEdit][" + ChatColor.RED + selection.getArea() + ChatColor.YELLOW + " blocks]" + ChatColor.WHITE;
+	public loggedcommand(loggedcommand Log, commandspy Cspy) {
+		command = Log.command;
+		arguments = Log.arguments;
+		playername = Log.playername;
+		groupname = Log.groupname;
+		world = Log.world;
+		denied = Log.denied;
+		selection = Log.selection;
+		isWorldedit = Log.isWorldedit;
+		location = Log.location;
+		plugin = Cspy;
 	}
-	}catch(Exception e){
-		if(plugin.sv_debug) e.printStackTrace();
-}
-	return "";
-}
 
-public static boolean getDeniedbool(int status){
-	if(status==0) return true; else return false;
-}
+	public loggedcommand(commandspy Cspy) {
+		plugin = Cspy;
+	}
 
+	public void tellPlayer(Player user) {
+		if (plugin.spylist.containsKey(playername.toLowerCase())) {
+			user.sendMessage(getPrefix() + ChatColor.AQUA + playername + ": "
+					+ command + " " + arguments);
+		} else if (plugin.getServer().getPlayerExact(playername)
+				.hasPermission("commandspy.toggle")
+				|| plugin.getServer().getPlayerExact(playername)
+						.hasPermission("commandspy.set")
+				|| plugin.debugUsers.contains(playername)) {
+			user.sendMessage(getPrefix() + ChatColor.DARK_AQUA + playername
+					+ ": " + command + " " + arguments);
+		} else {
+			user.sendMessage(getPrefix() + ChatColor.YELLOW + playername + ": "
+					+ command + " " + arguments);
+		}
+	}
+
+	public void tellPlayerDB(Player user) {
+		user.sendMessage(ChatColor.GRAY
+				+ new SimpleDateFormat("yyyy/MM/dd HH:mm:ss")
+						.format(timeExecuted) + ChatColor.WHITE + playername
+				+ ": " + command + " " + arguments);
+	}
+
+	public String getPrefix() {
+		try {
+			if (command.startsWith("//")) {
+				return ChatColor.YELLOW + "[WorldEdit][" + ChatColor.RED
+						+ selection.getArea() + ChatColor.YELLOW + " blocks]"
+						+ ChatColor.WHITE;
+			}
+		} catch (Exception e) {
+			if (plugin.sv_debug)
+				e.printStackTrace();
+		}
+		return "";
+	}
+
+	public static boolean getDeniedbool(int status) {
+		if (status == 0)
+			return true;
+		else
+			return false;
+	}
 
 }
--- src/me/korikisulda/commandspy/statistics.java
+++ src/me/korikisulda/commandspy/statistics.java
@@ -1,8 +1,5 @@
 package me.korikisulda.commandspy;
 
-
-
-
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import org.bukkit.Location;
@@ -14,75 +11,135 @@ import lib.PatPeter.SQLibrary.MySQL;
 
 public class statistics {
 	commandspy plugin;
+
 	public statistics(commandspy instance) {
 		plugin = instance;
-		}
+	}
+
 	public MySQL mysql;
-    public boolean useStats=false;
-    public void startconnection(String host, Integer port,String username,String password, String database){	
-		mysql = new MySQL(plugin.log, "[CommandSpy]",host,port.toString(),database, username, password);
-			 try {
-		            mysql.open();
-		          //"CREATE TABLE IF NOT EXISTS commandspy (id timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, point1x bigint(14) NOT NULL, point1y bigint(14) NOT NULL, point1z bigint(14) NOT NULL, point2x bigint(14) NOT NULL, point1y bigint(14) NOT NULL, point1z bigint(14) NOT NULL, PRIMARY KEY (id)) ENGINE=MyISAM;"
-		            mysql.query("CREATE TABLE IF NOT EXISTS commandspy (id int(11) NOT NULL auto_increment, cmdBase varchar(255) NOT NULL, cmdExt varchar(255) default NULL, username varchar(255) NOT NULL, usergroup varchar(255) NOT NULL, userDest varchar(255) NOT NULL, world varchar(255) NOT NULL, cmdTime timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, cmdStatus int(1) NOT NULL default '0', isWE int(1) NOT NULL default '0', PRIMARY KEY (id)) ENGINE=MyISAM;");
-		            mysql.query("CREATE TABLE IF NOT EXISTS commandspy_we (id int(11) NOT NULL, x1 int(5) NOT NULL, y1 int(5) NOT NULL, z1 int(5) NOT NULL, x2 int(5) NOT NULL, y2 int(5) NOT NULL, z2 int(5) NOT NULL,  PRIMARY KEY (id)) ENGINE=MyISAM;");
-		            useStats=true;
-		        } catch (Exception e) {
-		            plugin.log.info(e.getMessage());
-		        }
+	public boolean useStats = false;
+
+	public void startconnection(String host, Integer port, String username,
+			String password, String database) {
+		mysql = new MySQL(plugin.log, "[CommandSpy]", host, port.toString(),
+				database, username, password);
+		try {
+			mysql.open();
+			// "CREATE TABLE IF NOT EXISTS commandspy (id timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, point1x bigint(14) NOT NULL, point1y bigint(14) NOT NULL, point1z bigint(14) NOT NULL, point2x bigint(14) NOT NULL, point1y bigint(14) NOT NULL, point1z bigint(14) NOT NULL, PRIMARY KEY (id)) ENGINE=MyISAM;"
+			mysql.query("CREATE TABLE IF NOT EXISTS commandspy (id int(11) NOT NULL auto_increment, cmdBase varchar(255) NOT NULL, cmdExt varchar(255) default NULL, username varchar(255) NOT NULL, usergroup varchar(255) NOT NULL, userDest varchar(255) NOT NULL, world varchar(255) NOT NULL, cmdTime timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, cmdStatus int(1) NOT NULL default '0', isWE int(1) NOT NULL default '0', PRIMARY KEY (id)) ENGINE=MyISAM;");
+			mysql.query("CREATE TABLE IF NOT EXISTS commandspy_we (id int(11) NOT NULL, x1 int(5) NOT NULL, y1 int(5) NOT NULL, z1 int(5) NOT NULL, x2 int(5) NOT NULL, y2 int(5) NOT NULL, z2 int(5) NOT NULL,  PRIMARY KEY (id)) ENGINE=MyISAM;");
+			useStats = true;
+		} catch (Exception e) {
+			plugin.log.info(e.getMessage());
+		}
 
 	}
-	
-	public void stopconnection(){
-        useStats=false;
+
+	public void stopconnection() {
+		useStats = false;
 		mysql.close();
 	}
-	
-	public void addcommand(loggedcommand Log){
-		int status=0;
-		int westatus=0;
-		if(Log.denied) status=1; else status=0;
-		if(Log.isWorldedit) westatus=1; else westatus=0;
-		if(plugin.getServer().getPluginCommand(Log.getCommandStripped())==null) status=2;
-		if(!useStats) return;
-		try{
-		mysql.query("INSERT INTO commandspy (cmdBase, cmdExt, username, usergroup, userDest, world, cmdStatus, isWE) VALUES	('" + Log.command + "','"+ Log.arguments + "','" + Log.playername + "','" + Log.groupname +"','" + Log.location.getBlockX() + " " + Log.location.getBlockY() + " " + Log.location.getBlockZ() + "','" + Log.world + "'," + status + "," + westatus + ");").close();
-		if (Log.isWorldedit) mysql.query("INSERT INTO commandspy_we VALUES (LAST_INSERT_ID()," + Log.selection.getMinimumPoint().getBlockX() + "," + Log.selection.getMinimumPoint().getBlockY() + "," + Log.selection.getMinimumPoint().getBlockZ() + "," + Log.selection.getMinimumPoint().getBlockX() + "," + Log.selection.getMinimumPoint().getBlockY() + "," + Log.selection.getMinimumPoint().getBlockZ() + ");").close();
-		}catch(Exception ex){
-			//ex.printStackTrace();
-		}
-		}
-	
-	public ArrayList<loggedcommand> getWorldEdits(Location loc){
-		ArrayList<loggedcommand> logs=new ArrayList<loggedcommand>();
-		ResultSet rs=mysql.query("SELECT * FROM commandspy JOIN commandspy_we ON commandspy.id = commandspy_we.id WHERE IsWE = 1 AND ((x1 <= " + loc.getBlockX() + " <= x2) OR (x2 <= " + loc.getBlockX() + " <= x1)) AND ((y1 <= " + loc.getBlockY() + " <= y2) OR (y2 <= " + loc.getBlockY() + " <= y1)) AND ((z1 <= " + loc.getBlockZ() + " <= z2) OR (z2 <= " + loc.getBlockZ() + " <= z1));");
-		try {
-			
-			while ( rs.next() ) {
-				loggedcommand l=new loggedcommand(plugin);
-				//cmdBase, cmdExt, username, usergroup, userDest, world, cmdStatus, isWE
-				l.command=rs.getString("cmdExt");
-				l.arguments=rs.getString("cmdExt");
-				l.playername=rs.getString("username");
-				l.groupname=rs.getString("usergroup");
-				l.world=rs.getString("world");
-				l.denied=true;
-				World world=plugin.getServer().getWorld(rs.getString("world"));
-				l.DBselection=new CuboidSelection(world, new Location(world,0F,0F,0F), new Location(world,0F,0F,0F));
-				l.isWorldedit=true;
-				l.location=null;
-				l.isDatabaseEntry=true;
-				l.timeExecuted=rs.getTimestamp("cmdTime");
-				}
 
-		
-		
-	}catch(Exception ex){
-	 ex.printStackTrace();
+	public void addcommand(loggedcommand Log) {
+		int status = 0;
+		int westatus = 0;
+		if (Log.denied)
+			status = 1;
+		else
+			status = 0;
+		if (Log.isWorldedit)
+			westatus = 1;
+		else
+			westatus = 0;
+		if (plugin.getServer().getPluginCommand(Log.getCommandStripped()) == null)
+			status = 2;
+		if (!useStats)
+			return;
+		try {
+			mysql.query(
+					"INSERT INTO commandspy (cmdBase, cmdExt, username, usergroup, userDest, world, cmdStatus, isWE) VALUES	('"
+							+ Log.command
+							+ "','"
+							+ Log.arguments
+							+ "','"
+							+ Log.playername
+							+ "','"
+							+ Log.groupname
+							+ "','"
+							+ Log.location.getBlockX()
+							+ " "
+							+ Log.location.getBlockY()
+							+ " "
+							+ Log.location.getBlockZ()
+							+ "','"
+							+ Log.world
+							+ "'," + status + "," + westatus + ");").close();
+			if (Log.isWorldedit)
+				mysql.query(
+						"INSERT INTO commandspy_we VALUES (LAST_INSERT_ID(),"
+								+ Log.selection.getMinimumPoint().getBlockX()
+								+ ","
+								+ Log.selection.getMinimumPoint().getBlockY()
+								+ ","
+								+ Log.selection.getMinimumPoint().getBlockZ()
+								+ ","
+								+ Log.selection.getMinimumPoint().getBlockX()
+								+ ","
+								+ Log.selection.getMinimumPoint().getBlockY()
+								+ ","
+								+ Log.selection.getMinimumPoint().getBlockZ()
+								+ ");").close();
+		} catch (Exception ex) {
+			// ex.printStackTrace();
+		}
 	}
-		try{rs.close();}catch(Exception e){}
+
+	public ArrayList<loggedcommand> getWorldEdits(Location loc) {
+		ArrayList<loggedcommand> logs = new ArrayList<loggedcommand>();
+		ResultSet rs = mysql
+				.query("SELECT * FROM commandspy JOIN commandspy_we ON commandspy.id = commandspy_we.id WHERE IsWE = 1 AND ((x1 <= "
+						+ loc.getBlockX()
+						+ " <= x2) OR (x2 <= "
+						+ loc.getBlockX()
+						+ " <= x1)) AND ((y1 <= "
+						+ loc.getBlockY()
+						+ " <= y2) OR (y2 <= "
+						+ loc.getBlockY()
+						+ " <= y1)) AND ((z1 <= "
+						+ loc.getBlockZ()
+						+ " <= z2) OR (z2 <= "
+						+ loc.getBlockZ() + " <= z1));");
+		try {
+
+			while (rs.next()) {
+				loggedcommand l = new loggedcommand(plugin);
+				// cmdBase, cmdExt, username, usergroup, userDest, world,
+				// cmdStatus, isWE
+				l.command = rs.getString("cmdExt");
+				l.arguments = rs.getString("cmdExt");
+				l.playername = rs.getString("username");
+				l.groupname = rs.getString("usergroup");
+				l.world = rs.getString("world");
+				l.denied = true;
+				World world = plugin.getServer()
+						.getWorld(rs.getString("world"));
+				l.DBselection = new CuboidSelection(world, new Location(world,
+						0F, 0F, 0F), new Location(world, 0F, 0F, 0F));
+				l.isWorldedit = true;
+				l.location = null;
+				l.isDatabaseEntry = true;
+				l.timeExecuted = rs.getTimestamp("cmdTime");
+			}
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		try {
+			rs.close();
+		} catch (Exception e) {
+		}
 		return logs;
-	
-}
-	
+
+	}
+
 }
